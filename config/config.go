42165869bfcc9b22e5d6fda063f94028ec6f0ab6
Mark Wilkerson <mark@markwilkerson.me>
2017-03-04 15:29:33 -0800

    Add suuuuper basic config validation
    Added Path to Config for better Error output on validator

--- config/config.go
+++ config/config.go
@@ -2,7 +2,10 @@
 package config
 
 import (
+	"fmt"
 	"io/ioutil"
+	"log"
+	"os"
 
 	yaml "gopkg.in/yaml.v2"
 )
@@ -10,6 +13,7 @@ import (
 // Config models a config file
 type Config struct {
 	Sites []siteConfig
+	Path  string
 }
 
 // siteConfig models an individual Site entry
@@ -18,15 +22,26 @@ type siteConfig struct {
 }
 
 // Read reads the config.yaml file
+// this acts like a constructor
 func Read(path string) *Config {
 	file, err := ioutil.ReadFile(path)
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
 	c := new(Config)
 	err = yaml.Unmarshal(file, &c)
 	if err != nil {
 		panic(err)
 	}
+	c.Path = path
+	c.validate()
 	return c
+}
+
+// TODO make a better validator once I'm sure how config should be laid out
+func (c *Config) validate() {
+	if len(c.Sites) < 1 {
+		fmt.Printf("No valid config found at %s", c.Path)
+		os.Exit(1)
+	}
 }
