3dc7f458ccb8b1ed30c6046a4a6945482af568c7
Mark Wilkerson <mark@markwilkerson.me>
2017-03-04 14:53:33 -0800

    Add config flag
    Not super happy with this implementation

--- main.go
+++ main.go
@@ -1,1 +1,18 @@
 package main
+
+import (
+	"flag"
+	"fmt"
+
+	"github.com/markhuge/status/config"
+)
+
+func main() {
+	// Setup config
+	configFile := flag.String("f", "config.yml", "path to config file")
+	flag.Parse()
+
+	conf := config.Read(*configFile)
+	fmt.Printf("conf = %+v\n", conf)
+
+}
