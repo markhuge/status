99958e827d8d6289891eea75c7f5328f7996f403
Mark Wilkerson <mark@markwilkerson.me>
2017-03-04 12:28:40 -0800

    Implement Site struct

--- site/site.go
+++ site/site.go
@@ -1,1 +1,16 @@
+// package site provides methods for initiating status checks against websites
 package site
+
+// Site models a website to check
+type Site struct {
+
+	// URL specifies the HTTP/HTTPS URL to check
+	URL string
+
+	// ExpectedStatusCode is the HTTP status code used to determine "OK" status
+	// (default is 200)
+	ExpectedStatusCode int
+
+	// LastChecked is a timestamp of the last check run
+	LastChecked Time
+}
