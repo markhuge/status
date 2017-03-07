93e0e9dfd0e7a76565d3596a58fcffc99a7fdc81
Mark Wilkerson <mark@markwilkerson.me>
2017-03-06 20:49:17 -0800

    Holy shit golint is picky...

--- site/site.go
+++ site/site.go
@@ -1,4 +1,4 @@
-// package site provides methods for initiating status checks against websites
+// Package site provides methods for initiating status checks against websites
 package site
 
 import (
